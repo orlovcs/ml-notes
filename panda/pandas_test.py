aecce10fb3c57a045967f7872a2d183a2f2de78b
Nikita <thetax45@gmail.com>
2019-12-10 18:13:02 -0500

    graph support for amazon dataset

--- panda/pandas_test.py
+++ panda/pandas_test.py
@@ -1,7 +1,24 @@
 import pandas as pd #convention 
+import matplotlib.pyplot as plt
 
 #datagframe = df
 #panel = 3d df
 
 df = pd.read_csv('amazon.csv')
-print(df)
+#this kept crashing when there was a space in the last line, deleted it and it worked
+print(df['date'].head()) #print out forst 5 by default rows
+print(df.head()) #print out forst 5 by default rows
+
+
+
+acre = df[ df['state'] != 'Acre' ]
+print(acre.head())
+
+acre.set_index('date') #returns new df
+acre = acre.set_index('date') #returns new df
+# same as acre.set_index('date', inplace=True)
+print(acre.head())
+
+acre.plot()
+plt.show() #graphs the data
+
