8eb02c38190b1118b470c49c94a232d7905ce9aa
Nikita <thetax45@gmail.com>
2019-12-10 17:48:34 -0500

    added csv from kaggle

--- ml/regression.py
+++ ml/regression.py
@@ -2,9 +2,14 @@
 import pandas as pd
 import quandl 
 import math
+import datetime
 import numpy as np #computing library
 from sklearn import preprocessing, model_selection, svm # vector machine
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 
 quandl.ApiConfig.api_key = "9xfxcdQFN8z3rzEx89LH"
@@ -27,24 +32,45 @@ df['label'] = df[forecast_column].shift(-forecast_output) # we shift the data ba
 df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)  #scale data before classifier, scale new values along with training data
 #X = X[:-forecast_output+1] #only have X's when have values for ydf.dropna(inplace=True)
-#df.dropna(inplace=True)
+#X=X[:-forecast_output]
+X_lately=X[-forecast_output:]
+df.dropna(inplace=True)
 y = np.array(df['label'])
-
-#print(len(X), len(y))
+print(len(X), len(y))
 
 
 X_train,X_test,y_train,y_test = model_selection.train_test_split(X, y, test_size=0.2) # take features, labels, shuffles points and outputs
-clf =  LinearRegression()
+clf =  LinearRegression(n_jobs=20) # switch algorithms, specifies threads 
 
 clf.fit(X_train,y_train) # fit = train
 
 accuracy = clf.score(X_test, y_test) # score = test
 
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, accuracy, forecast_output)
+
  #trains and test seperate data because if training classifier to predict data you're testing against the results will be the same
 
-print(accuracy) # what the accuracy would be predicting the price shift by the end of the day
+#print(accuracy) # what the accuracy would be predicting the price shift by the end of the day
 # accuracy is squared error
 
+# for each algo, look of it can be threaded in the documentation, amount of n_jobs that can be dedicated to it
+# linear regression much more threadable
+
+df['Forecast'] = np.nan # empty data
+last_date = df.iloc[-1].name
+last_unixval = last_date.timestamp()
+one_day = 86400
+next_unixval = last_unixval + one_day
+
+#x,y don't necessarily correspond to axis, X/features & y/label
+# label happens to be the price so y is the correct axis
+
+for i in forecast_set:
+   next_date = datetime.datetime.fromtimestamp(next_unixval)
+   next_unixval += one_day
+   df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + i
+   # iterate through forecase set, take each forecast and day and set them as values in data frame, make future features not a number
---
+++ panda/pandas_test.py
@@ -0,0 +1,7 @@
+import pandas as pd #convention 
+
+#datagframe = df
+#panel = 3d df
+
+df = pd.read_csv('amazon.csv')
+print(df)
